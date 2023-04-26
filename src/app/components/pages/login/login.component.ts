c84cb03696291d6ea395701fbbdd52a999ba7045
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-04-26 19:24:45 -0300

    feat: add login page

---
+++ src/app/components/pages/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isLoading: boolean = false;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required]),
+    });
+  }
+
+  get email() {
+    return this.loginForm.get("email");
+  }
+
+  get password() {
+    return this.loginForm.get("password");
+  }
+
+  handleLogin(): void {
+    if (this.loginForm.invalid) return;
+
+    this.isLoading = true;
+
+    console.log(this.loginForm);
+  }
+}
