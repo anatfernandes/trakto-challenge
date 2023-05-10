280c92b87e7f8c67060eaac15a356776cce8bf40
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-05-10 17:05:37 -0300

    feat: list designs in courseware all page

--- src/app/components/pages/courseware-all/courseware-all.component.ts
+++ src/app/components/pages/courseware-all/courseware-all.component.ts
@@ -1,8 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Design } from "src/app/interfaces/Design";
+import { DesignEntity } from "src/app/interfaces/DesignEntity";
+import { DesignsService } from "src/app/services/designs/designs.service";
+import { MessagesService } from "src/app/services/messages/messages.service";
 
 @Component({
   selector: "app-courseware-all",
   templateUrl: "./courseware-all.component.html",
   styleUrls: ["./courseware-all.component.css"],
 })
-export class CoursewareAllComponent {}
+export class CoursewareAllComponent implements OnInit {
+  designs: Design[] = [];
+
+  constructor(
+    private designsService: DesignsService,
+    private messagesService: MessagesService
+  ) {}
+
+  ngOnInit() {
+    this.designsService.listAll().subscribe({
+      next: (response: DesignEntity) => {
+        this.designs = response.data;
+      },
+      error: () => {
+        this.messagesService.create(
+          "Não foi possível carregar os designs! Por favor, tente novamente.",
+          "error"
+        );
+      },
+    });
+  }
+}
